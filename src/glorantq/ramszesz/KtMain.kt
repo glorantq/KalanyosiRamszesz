989eda00c25b9965687040df2082a7954e014307
Gerber Lóránt Viktor <glorantq@gmail.com>
2017-07-24 13:35:47 +0200

    Added Unshorten Command

--- src/glorantq/ramszesz/KtMain.kt
+++ src/glorantq/ramszesz/KtMain.kt
@@ -5,6 +5,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 val inviteUrl: String = "https://discordapp.com/api/oauth2/authorize?client_id=338377690002489344&scope=bot&permissions=356936710"
+val discordToken: String = "MzM4Mzc3NjkwMDAyNDg5MzQ0.DFX8zQ._p2A1-"
 
 fun main(args: Array<String>) {
     (LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME) as ch.qos.logback.classic.Logger).level = Level.DEBUG
